ea75ae19d3d1d983a810b94f7ca734806d0d51f9
Radhadasa09 <greatgirish@gmail.com>
2026-08-22 03:09:39 +0530

    Update store_app.py

--- store_app.py
+++ store_app.py
@@ -17,9 +17,9 @@ import cloudinary.uploader
 
 # --- SECURE CLOUDINARY INIT ---
 cloudinary.config(
-    cloud_name = st.secrets["cloudinary"]["cloud_name"],
-    api_key = st.secrets["cloudinary"]["api_key"],
-    api_secret = st.secrets["cloudinary"]["api_secret"],
+    cloud_name = st.secrets["CLOUDINARY_CLOUD_NAME"],
+    api_key = st.secrets["CLOUDINARY_API_KEY"],
+    api_secret = st.secrets["CLOUDINARY_API_SECRET"],
     secure = True
 )
 
